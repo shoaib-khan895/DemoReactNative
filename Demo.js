699d92879cc95eb99d65ee717c5dbff89c13233d
shoaib-khan895 <71784547+shoaib-khan895@users.noreply.github.com>
2022-08-24 17:01:25 +0530

    Component usage

--- Demo.js
+++ Demo.js
@@ -1,39 +1,41 @@
-import React, {Component} from 'react';
-import {Text,View,Button} from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, Button, SafeAreaView } from 'react-native';
 
 class DemoTwo extends Component {
-  
-  constructor() {
-    super();
-    this.state={
-      data: "Click to update"
+
+    constructor() {
+        super();
+        this.state = {
+            data: "Click to update"
+        }
+        console.warn("--> constructor is called");
     }
-   console.warn("--> constructor is called");
-  }
-  componentDidMount(){
-    // API calls here!!
-    console.warn("--> componentDidMount is called");
-  }
-  componentDidUpdate(){
-    console.warn("-> componentDidUpdate is called");
-  }
-  shouldComponentUpdate(props, state){
-    console.warn("--> shouldComponentUpdate is called");
-    return true;
-  }
-  componentDidUpdate(){
-    console.warn("--> componentDidUpdate is called");
-  }
-  render() 
-  {
-    console.warn("--> render is called");
-    return (
-      <View>
-        <Text>Hello World 2</Text>
-        <Button title={this.state.data} onPress={()=>{this.setState({data:"You updated me"})}}></Button>
-      </View>
-    );
-  }
+    componentDidMount() {
+        // API calls here!!
+        console.warn("--> componentDidMount is called");
+    }
+    componentDidUpdate() {
+        console.warn("-> componentDidUpdate is called");
+    }
+    shouldComponentUpdate(props, state) {
+        console.warn("--> shouldComponentUpdate is called");
+        return true;
+    }
+    componentDidUpdate() {
+        console.warn("--> componentDidUpdate is called");
+    }
+    render() {
+        console.warn("--> render is called");
+        return (
+            <SafeAreaView>
+                <View>
+                    <Text>Hello World 2</Text>
+                    <Button title={this.state.data} onPress={() => { this.setState({ data: "You updated me" }) }}></Button>
+                </View>
+            </SafeAreaView>
+
+        );
+    }
 }
 
 
